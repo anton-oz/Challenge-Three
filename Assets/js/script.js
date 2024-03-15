229e49dcfc245c8bc9c78e116c45cf5ef2c82bba
anton <antonosland789@gmail.com>
2024-03-15 18:20:54 -0500

    added acceptance critera comment on script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,26 @@
+// GIVEN an employee payroll tracker
+// WHEN I click the "Add employee" button
+// THEN I am presented with a series of prompts asking for first name, last name, and salary
+// WHEN I finish adding an employee
+// THEN I am prompted to continue or cancel
+// WHEN I choose to continue
+// THEN I am prompted to add a new employee
+// WHEN I choose to cancel
+// THEN my employee data is displayed on the page sorted alphabetically by last name, and the console shows computed and aggregated data
+
+
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  if (addEmployeesBtn) {
+    prompt("Employee first name: ")
+    prompt("Employee last name: ")
+    prompt("Employee salary: ")
+    
+  }
 }
 
 // Display the average salary
--- README.md
+++ README.md
@@ -40,6 +40,18 @@ If you used any third-party assets that require attribution, list the creators w
 
 If you followed tutorials, include links to those here as well.
 
+Sources:
+
+[JavaScript how to get user input](https://codehs.com/tutorial/rachel/user-input-in-javascript)\
+
+[]()
+
+[]()
+
+[]()
+
+[]()
+
 ## License
 
 The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
